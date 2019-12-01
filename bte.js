6da44c7887905f842e281ed4934b462ac988e112
Tomas Slepikas <tomsle1@ktu.lt>
2019-12-01 16:56:11 +0200

    2nd commit

---
+++ bte.js
@@ -0,0 +1,37 @@
+
+console.log("Started bte.js")
+window.onload = function () {
+    console.log("window.onload passed")
+    document.getElementById("connectBtn").addEventListener('click', _ => {
+        // Request the device
+        mBot.request()
+            .then(_ => {
+                // Connect to the mbot
+                return mBot.connect();
+            })
+            .then(_ => {
+                // Connection is done, we show the controls
+                let connectedCard = document.getElementById("connectedAlert");
+                connectedCard.className = "alert alert-success";
+                alert("connected")
+
+                // Control the robot by button
+                let upBtn = document.getElementById("upBtn");
+                let downBtn = document.getElementById('downBtn');
+                let leftBtn = document.getElementById('leftBtn');
+                let rightBtn = document.getElementById('rightBtn');
+                upBtn.addEventListener('touchstart', _ => { mBot.processMotor(-250, 250) });
+                downBtn.addEventListener('touchstart', _ => { mBot.processMotor(250, -250) });
+                leftBtn.addEventListener('touchstart', _ => { mBot.processMotor(250, 250) });
+                rightBtn.addEventListener('touchstart', _ => { mBot.processMotor(-250, -250) });
+
+                upBtn.addEventListener('touchend', _ => { mBot.processMotor(0, 0) });
+                downBtn.addEventListener('touchend', _ => { mBot.processMotor(0, 0) });
+                leftBtn.addEventListener('touchend', _ => { mBot.processMotor(0, 0) });
+                rightBtn.addEventListener('touchend', _ => { mBot.processMotor(0, 0) });
+            })
+    })
+
+}
+
+
